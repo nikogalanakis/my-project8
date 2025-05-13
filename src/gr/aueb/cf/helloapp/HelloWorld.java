b8d3c4e1698c82089c186127c65ab6cd1269613d
galanakis.delfi@aueb.gr <galanakis.delfi@aueb.gr>
2025-05-13 20:56:36 +0300

    feat: add hello world

---
+++ src/gr/aueb/cf/helloapp/HelloWorld.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.helloapp;
+
+/**
+ * This proejct was created to show
+ * how to make a git project and then how
+ * to stage and commit changes in local repo.
+ *
+ */
+public class HelloWorld {
+}
